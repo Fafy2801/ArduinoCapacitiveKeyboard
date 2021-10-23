a0806f2a90ccb5a7830f47eff78465d38d0cb525
Fafy <fabianrojas2801@gmail.com>
2021-10-23 19:36:42 -0300

    Added Arduino file

---
+++ arduinoserial/arduinoserial.ino
@@ -0,0 +1,41 @@
+#include<CapacitiveSensor.h>
+
+int PIN_OUT = 3;
+
+int pins[4] = {5, 6, 10, 11};
+int leds[4] = {8, 9, 12, 4};
+
+int iterations = sizeof(pins) / sizeof(int);
+
+CapacitiveSensor sensors[] = {};
+
+void setup() {
+  Serial.begin(9600);
+
+  for(int i = 0; i < iterations; i++)
+    pinMode(leds[i], OUTPUT);
+
+  for(int i = 0; i < iterations; i++){
+      sensors[i] = CapacitiveSensor(PIN_OUT, pins[i]);
+      sensors[i].set_CS_AutocaL_Millis(0xFFFFFFFF);
+  }
+}
+
+long reading = 0;
+byte out = 0;
+
+void loop() {
+  out = 0;
+  for(int i = 0; i < iterations; i++){
+    reading = sensors[i].capacitiveSensor(100);
+
+    if(reading > 25){
+      bitSet(out, i);
+      digitalWrite(leds[i], HIGH);
+    } else
+      digitalWrite(leds[i], LOW);
+  }
+
+  Serial.write(out);
+  delay(10);
+}
